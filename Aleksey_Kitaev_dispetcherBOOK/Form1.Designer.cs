3463272fd641f9611c011f3f824b2be71157726b
ExHedge\Hedge <axlstarxx@gmail.com>
2021-03-13 17:28:04 +0300

    Добавьте файлы проекта.

---
+++ Aleksey_Kitaev_dispetcherBOOK/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Aleksey_Kitaev_dispetcherBOOK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Иваново";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Автоблокировка";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 57);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Московское направление";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 57);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Телефоны ЭЧС-2";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 57);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Прочие Иваново";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(218, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 516);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Книга диспетчера ЭЧС-2";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(542, 518);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 42);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Потребители";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(623, 518);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 42);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Схема";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(704, 518);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 42);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Печать";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(785, 518);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 42);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "Дополнительно";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 327);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(160, 57);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "Телефоны ЯДЭ";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 453);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(160, 57);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "***";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 390);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(160, 57);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "Прочие службы";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(543, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(317, 464);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 568);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Книга диспетчера ЭЧС-2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Aleksey_Kitaev_dispetcherBOOK/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aleksey_Kitaev_dispetcherBOOK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            string[] substations = { "ТП-40", "ТП-69", "ТП-95", "ТП-660", "ТП-816", "ТП-816", "ТП-912", "ТП-395", "ТП-401", "ТП-460",
+                "ТП-467", "ТП - 468", "ТП-469", "ТП - 477", "ТП - 672",
+                "ТП - 717", "ТП - 729", "ТП - 825", "ТП - 879", "ТП - 892"};
+            listBox1.Items.AddRange(substations);
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+         // substations.
+        }
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          //  listBox1.Items[0]
+            string selectedCountry = listBox1.SelectedItem.ToString();
+            textBox1.Text = (selectedCountry);
+        }
+    }
+}
